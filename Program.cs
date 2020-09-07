b8f7384fcafc8219215f26a553c3012661b6dd60
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-09-07 21:42:18 +0600

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace HW_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SqlConnectionStringBuilder connect =
+                new SqlConnectionStringBuilder();
+            connect.InitialCatalog = "Autolot";
+            connect.DataSource = @"(local)\SQLEXPRESS";
+            connect.ConnectTimeout = 30;
+            connect.IntegratedSecurity = true;
+
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = connect.ConnectionString;
+                cn.Open();
+                cn.Close();
+            }
+        }
+    }
+}
